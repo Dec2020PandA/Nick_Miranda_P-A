c48eac7e7dc761d8a37b368460b22df1c544c8ca
Nick Miranda <nicolasamiranda97@gmail.com>
2020-12-15 18:14:37 -0500

    Solo Project

---
+++ Solo Project/ClientCaseData/src/main/java/com/nick/ClientCaseData/repositories/ClientRepository.java
@@ -0,0 +1,21 @@
+package com.nick.ClientCaseData.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nick.ClientCaseData.models.Client;
+import com.nick.ClientCaseData.models.Member;
+
+@Repository 
+public interface ClientRepository extends CrudRepository<Client, Long> {
+
+	List<Client> findAll();
+	
+//	List<Client> findAllByOrderByFirstNameAsc();
+	
+	List<Client> findByNameContaining(String name);
+	
+	List<Client> findByMembersNotContains(Member member);
+}
